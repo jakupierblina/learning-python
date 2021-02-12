95d6242683879142eaedf55956d650fd56d4789e
Erblina Jakupi <higo5t@INF.ELTE.HU>
2021-02-12 17:32:36 +0100

    login-signup design

--- dailytrackapp/dailytrackapp/urls.py
+++ dailytrackapp/dailytrackapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webdemo.urls')),
-    path('login', include('webdemo.urls')),
-    path('register', include('webdemo.urls')),
+    path('login/', include('webdemo.urls')),
+    path('register/', include('webdemo.urls')),
+    path('', include("django.contrib.auth.urls")),
 ]
--- dailytrackapp/webdemo/models.py
+++ dailytrackapp/webdemo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#user's class
+
+
+
+#class todo
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
